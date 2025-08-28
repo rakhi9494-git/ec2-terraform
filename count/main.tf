c33e538cb3269f14f2c93f1a29eb01bb03f31c80
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2025-08-28 15:27:26 +0530

    Create main.tf

---
+++ count/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-southeast-2" # Or your desired region
+}
+
+
+variable "instance_count" {
+  description = "The number of EC2 instances to create."
+  type        = number
+  default     = 2 # This will create two instances
+}
+
+# Create EC2 instances using the count meta-argument
+resource "aws_instance" "example" {
+
+  count = var.instance_count 
+  ami           = "ami-0a0b0b06dd1636865"
+  key_name = "rajdevops"
+  
+
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "My-EC2-Instance-${count.index}"
+  }
+}
+
+output "instance_public_ips" {
+  description = "The public IP addresses of the created instances."
+  value       = aws_instance.example[*].public_ip
+}
